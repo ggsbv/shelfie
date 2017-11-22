d9082caf26219a92f6951fd4cc386494ab62f5eb
misssrakiep <shameerarakiep@gmail.com>
2017-11-22 11:08:14 +0200

    changes made to original header

---
+++ bookDisplay.js
@@ -0,0 +1,39 @@
+// get all books to display on the home screen
+$.ajax({
+  type : "GET",
+url : /*sill to be added*/
+dataType : "json",
+success : function (books) {
+  console.log(books);
+  ko.observable({
+    //data that is grabbed from the database
+  });
+}
+});
+
+$.ajax({
+  type: "GET",
+url: /*still to be added*/
+dataType: "json",
+success: function (books) {
+  console.log(books);
+  ko.observable({
+    //books that meet search bar specs
+  });
+}
+});
+
+$.ajax({
+  type : "POST",
+url : /*still to be added*/
+dataType : "json"
+success : function(books) {
+  console.log(arguments);
+
+ko.observable({
+  /*book count on that specific book will decrease*/
+})
+
+  var initDocument = $.extend(true, {}, document);
+}
+})
